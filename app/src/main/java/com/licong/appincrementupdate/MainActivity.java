2d7193d40fdc04ed3278e3dd4109bcb4fce41d1c
Raintail <568027479@qq.com>
2019-07-04 11:13:18 +0800

    add android demo
    Signed-off-by: Raintail <568027479@qq.com>

---
+++ app/src/main/java/com/licong/appincrementupdate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.licong.appincrementupdate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ tools/readme.txt
@@ -0,0 +1,8 @@
+
+生成增量文件
+./bsdiff old.apk new.apk old-to-new.patch
+
+增量文件和old.apk合并成新的apk
+./bspatch old.apk new2.apk old-to-new.patch
+
+
